e179b18ccad4ce89c83a33b8996c3b83c71efb25
JaimedeMiguel <jaimedemiguelsh@gmail.com>
2022-11-29 19:10:20 +0100

    Implementacio Vue

---
+++ frontend/src/components/Header/UnauthorizedHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="Header--un">
+    <FoovieLogo class="Header__logo Header__logo--un" />
+    <div class="Header__actions">
+      <router-link v-if="routePath !== signInRoute" class="btn btn--primary" :to="signInRoute">
+        Sign in
+      </router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+  import FoovieLogo from '../../assets/images/Foovie.svg';
+  import { routes } from '../../helpers/constants';
+
+  export default {
+    name: 'UnauthorizedHeader',
+    data() {
+      return {
+        signInRoute: routes.signIn,
+      };
+    },
+    computed: {
+      routePath() {
+        return this.$route.path;
+      },
+    },
+    components: {
+      FoovieLogo,
+    },
+  };
+</script>
+
+<style lang="scss">
+  @import "./Header.scss";
+</style>
